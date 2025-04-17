a595a48247dba7d1d3e234fc6085062625a504c4
Dani <daisuke.wi@gmail.com>
2025-04-17 10:48:04 +0200

    Documment the new controlled gate application and remove the multicontrolled gate application because it is not working.

--- qregistry.py
+++ qregistry.py
@@ -70,37 +70,6 @@ class QRegistry:
         for d in range(distance):
             log("x SWAP", end=" ")
             self.__apply_gate(Gate.SWAP().matrix, target - distance + d + gate_distance)
-
-    def apply_mc_gate(self, gate : Gate, target : int, controls : list[int]):
-        if len(controls) == 0:
-            return self.apply_gates(gate, target)
-        
-        gate_matrix = sp.coo_matrix([1])
-        
-        # Create the controlled gate matrix
-        for i in range(self.n_qubits):
-            if i in controls:
-                gate_matrix = sp.kron(sp.coo_matrix([[1, 0], [0, 0]]), gate_matrix, format=gate_matrix.format)
-            elif i == target:
-                gate_matrix = sp.kron(gate.matrix, gate_matrix, format=gate_matrix.format)
-            else:
-                gate_matrix = sp.kron(sp.identity(2, dtype=complex), gate_matrix, format=gate_matrix.format)
-
-        # Add the control condition
-        for i in range(2**(self.n_qubits - 1)):
-            binary = bin(i)[2:].zfill(self.n_qubits)
-            control_match = True
-            for c in controls:
-                if binary[c] == '0':
-                    control_match = False
-                    break
-            if control_match:
-                continue
-            gate_matrix[i, i] = 1
-
-        self.__apply_gate(gate_matrix, target)
-
-        return
     
     def apply_gates(self, gates: Union[List[Gate], Gate], targets: Union[List[int], int]):
         if isinstance(targets, int):
